5e83e4f256bf5f7a1c4a700a6cc0d1bd024b64a7
Marco Brasci <marco_brasci@hotmail.com>
2014-10-29 18:42:50 +0100

    Calendar view added

--- js/parallax-scrolling.js
+++ js/parallax-scrolling.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-
+	
 
 	if (!Modernizr.touch) { 
-		alert('No Touch Screen');
+
    // cache the window object
    $window = $(window);
 
@@ -14,8 +14,6 @@ $(document).ready(function(){
         // HTML5 proves useful for helping with creating JS functions!
         // also, negative value because we're scrolling upwards    
 
-
-
 		/**
 		How many pixels we are from the top of the page
 		*/
@@ -25,27 +23,35 @@ $(document).ready(function(){
 		*/
 		var speed = $scroll.data('speed');
 		
-		console.log("$window.scrollTop(): " + $window.scrollTop());
-		console.log("$scroll.data('speed'): " + $scroll.data('speed'));
+
 		
 		var yPos = -(pixelsToTopOfPage / speed);
 
-		console.log("yPos: " + yPos);
+		
         // background position
         var coords = '50% '+ yPos + 'px';
 
-        console.log("New coords: " + coords)
+        
         // move the background
-        $scroll.css({ backgroundPosition: coords });   
+        $scroll.css({ backgroundPosition: coords }); 
 
+
+        var debug = true;
+        if(debug){
+        	console.log("yPos: " + yPos);
+        	console.log("New coords: " + coords);
+        	console.log("$window.scrollTop(): " + $window.scrollTop());
+        	console.log("$scroll.data('speed'): " + $scroll.data('speed'));
+        }
 
       }); // end window scroll
    });  // end section function
 
 }
 else { 
-	alert('Touch Screen');
+	
 }
 
 });
+
 
